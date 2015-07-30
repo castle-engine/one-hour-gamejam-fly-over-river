718397b9734b1a6eae5b0eb453c67e20428c415b
michaliskambi <michalis.kambi@gmail.com>
2015-07-30 22:46:26 +0200

    Lots of code cleanups, TEntity object, fixed consistent collisions margins.

--- game.pas
+++ game.pas
@@ -5,192 +5,201 @@ unit Game;
 
 interface
 
-uses CastleWindowTouch;
+uses CastleWindow;
 
 var
-  Window: TCastleWindowTouch;
+  Window: TCastleWindow;
 
 implementation
 
-uses SysUtils, CastleWindow, CastleScene, CastleControls,
-  CastleFilesUtils, CastleSceneCore, CastleKeysMouse,
+uses SysUtils,
+  CastleFilesUtils, CastleKeysMouse,
   CastleImages, CastleGLImages, CastleVectors, CastleMessages;
 
 type
-  TEnemy = object
-    X, Y: Integer;
+  { Entity in a world, like a player or enemy or rocket. }
+  TEntity = object
+    Position: TVector2Single;
     Alive: boolean;
+    Image: TGLImage;
+    function Collides(const E: TEntity): boolean;
+    function Width: Integer;
+    function Height: Integer;
   end;
 
-var
-  MapWidth, MapHeight: Integer;
-  Map, Player, Enemy, Rocket: TGLImage;
-  MapMask: TCastleImage;
-  Position: TVector2Single;
-  Enemies: array [0..50] of TEnemy;
-  Rockets: array [0..40] of TEnemy;
+function TEntity.Collides(const E: TEntity): boolean;
+begin
+  Result :=
+    Alive and
+    E.Alive and
+    { note: use "div 3", not "div 2" below, to collide only when things
+       are really close, not when merely their bounding boxes collide. }
+    (Abs(Position[0] - E.Position[0]) < (Width  + E.Width ) div 3) and
+    (Abs(Position[1] - E.Position[1]) < (Height + E.Height) div 3);
+end;
 
-{ routines ------------------------------------------------------------------- }
+function TEntity.Width: Integer;
+begin
+  Result := Image.Width;
+end;
+
+function TEntity.Height: Integer;
+begin
+  Result := Image.Height;
+end;
+
+var
+  Map, PlayerImage, Enemy, Rocket: TGLImage;
+  MapMask: TGrayscaleImage;
+  Player: TEntity;
+  Enemies: array [0..50] of TEntity;
+  Rockets: array [0..40] of TEntity;
 
 procedure Restart;
 var
   I: Integer;
 begin
-  Position := Vector2Single(MapWidth / 2 - 100, Player.Height / 2);
+  Player.Alive := true;
+  Player.Image := PlayerImage;
+  Player.Position := Vector2Single(Map.Width / 2 - 100, Player.Height);
+
   for I := 0 to High(Enemies) do
   begin
     Enemies[I].Alive := true;
-    Enemies[I].X := MapWidth div 4 +  Round(Random * MapWidth / 2);
-    Enemies[I].Y := Round(Random * MapHeight * 3);
+    Enemies[I].Image := Enemy;
+    Enemies[I].Position := Vector2Single(
+      Map.Width div 4 + Random * Map.Width / 2,
+      Random * Map.Height * 3
+    );
   end;
 
   for I := 0 to High(Rockets) do
+  begin
     Rockets[I].Alive := false;
+    Rockets[I].Image := Rocket;
+  end;
 end;
 
 { One-time initialization of resources. }
 procedure ApplicationInitialize;
 begin
   Map := TGLImage.Create(ApplicationData('map.png'));
-  MapMask := LoadImage(ApplicationData('map_mask.png'), []);
-  MapWidth := Map.Width;
-  MapHeight := Map.Height;
-
-  Player := TGLImage.Create(ApplicationData('SpaceShipSmall.png'));
-
+  MapMask := LoadImage(ApplicationData('map_mask.png'), [TGrayscaleImage]) as TGrayscaleImage;
+  PlayerImage := TGLImage.Create(ApplicationData('SpaceShipSmall.png'));
   Enemy := TGLImage.Create(ApplicationData('ship6c.png'));
-
   Rocket := TGLImage.Create(ApplicationData('cohete_off.png'));
 
   Restart;
 end;
 
-
-procedure WindowResize(Container: TUIContainer);
-begin
-  // ... react to Container Width / Height changes
-end;
-
+{ Window.OnRender callback. }
 procedure WindowRender(Container: TUIContainer);
+
+  function ShiftY: Single;
+  begin
+    Result := -Player.Position[1] + 1.5 * Player.Height;
+  end;
+
+  procedure DrawEntity(const E: TEntity);
+  begin
+    if E.Alive then
+      E.Image.Draw(
+        Round(E.Position[0] - E.Width div 2),
+        Round(E.Position[1] - E.Height div 2 + ShiftY)
+      );
+  end;
+
 var
-  I, X, Y: Integer;
+  I: Integer;
 begin
   // TODO: couple of times render map
   for I := 0 to 3 do // TODO: unoptimal, only 2 draws should be needed
-    Map.Draw(0, -Round(Position[1]) + Map.Height * I);
-  Player.Draw(Round(Position[0]), Player.Height div 2);
+    Map.Draw(0, Round(ShiftY + Map.Height * I));
+
+  DrawEntity(Player);
 
   for I := 0 to High(Enemies) do
-    if Enemies[I].Alive then
-    begin
-      Enemy.Draw(
-        Enemies[I].X - Enemy.Width div 2,
-        Enemies[I].Y - Enemy.Height div 2 +
-        -Round(Position[1]));
-    end;
+    DrawEntity(Enemies[I]);
 
   for I := 0 to High(Rockets) do
-    if Rockets[I].Alive then
-    begin
-      if Rockets[I].Y > (MapHeight * 3) then
+  begin
+    if Rockets[I].Alive and
+       (Rockets[I].Position[1] > (Map.Height * 3)) then
         // TODO: better condition to end rocket fly
-        Rockets[I].Alive := false else
-      begin
-        X := Rockets[I].X - Rocket.Width div 2;
-        Y := Rockets[I].Y - Rocket.Height div 2 - Round(Position[1]);
-        Rocket.Draw(X, Y);
-      end;
-    end;
+      Rockets[I].Alive := false;
+
+    DrawEntity(Rockets[I]);
+  end;
 end;
 
+{ Window.OnUpdate callback. }
 procedure WindowUpdate(Container: TUIContainer);
+
+  function PlayerCrashedWithWall(const X, Y: Single): boolean;
+  const
+    { For collisions of "player vs wall", we check points around player middle,
+      but not too far around, to not detect collisions too soon.
+      Set this to 1.0 to exactly check player bbox corners,
+      set to something smaller to give more chance to avoid collisions. }
+    PlayerMargin = 0.5;
+  var
+    MapX, MapY: Integer;
+  begin
+    MapX := Round(Player.Position[0] + X * PlayerMargin * Player.Width / 2);
+    MapY := Round(Player.Position[1] + Y * PlayerMargin * Player.Height / 2);
+    { is mask black }
+    Result := MapMask.PixelPtr(MapX mod Map.Width, MapY mod Map.Height)^ = 0;
+    if Result then
+    begin
+      MessageOk(Window, 'Crash!');
+      Restart;
+    end;
+  end;
+
+const
+  { speeds, in map pixels per second }
+  MoveSidewaysSpeed = 300;
+  MoveForwardSpeed = 500;
+  RocketSpeed = 2000;
 var
-  X, Y: Integer;
   I, J: Integer;
 begin
-  Position := Vector2Single(
-    Position[0],
-    Position[1] + Window.Fps.UpdateSecondsPassed * 500);
-  Window.Invalidate;
-
-//  if Window.Pressed[K_Up]    then ViewMoveY -= ViewMoveChangeSpeed * Window.Fps.UpdateSecondsPassed;
-//  if Window.Pressed[K_Down]  then ViewMoveY += ViewMoveChangeSpeed * Window.Fps.UpdateSecondsPassed;
-
+  { move player }
+  Player.Position += Vector2Single(0, Window.Fps.UpdateSecondsPassed * MoveForwardSpeed);
   if Window.Pressed[K_Right] then
-    Position := Vector2Single(
-      Position[0] + Window.Fps.UpdateSecondsPassed * 300,
-      Position[1]);
+    Player.Position += Vector2Single(Window.Fps.UpdateSecondsPassed * MoveSidewaysSpeed, 0);
   if Window.Pressed[K_Left]  then
-    Position := Vector2Single(
-      Position[0] - Window.Fps.UpdateSecondsPassed * 300,
-      Position[1]);
+    Player.Position -= Vector2Single(Window.Fps.UpdateSecondsPassed * MoveSidewaysSpeed, 0);
 
-  X := Round(Position[0] + Player.Width div 2);
-  Y := Round(Position[1] + Player.Height div 2);
+  { collisions enemies vs player }
   for I := 0 to High(Enemies) do
-    if Enemies[I].Alive and
-       (Abs(Enemies[I].X - X) < (Player.Width + Enemy.Width) div 3) and
-       (Abs(Enemies[I].Y - Y) < (Player.Height + Enemy.Height) div 3) then
+    if Player.Collides(Enemies[I]) then
     begin
       MessageOk(Window, 'Crash with enemy!');
       Restart;
+      Exit;
     end;
 
+  { collisions rockets vs enemies, move rockets }
   for I := 0 to High(Rockets) do
     if Rockets[I].Alive then
     begin
       for J := 0 to High(Enemies) do
-        if Enemies[J].Alive and
-           (Abs(Enemies[J].X - Rockets[I].X) < (Rocket.Width + Enemy.Width) div 3) and
-           (Abs(Enemies[J].Y - Rockets[I].Y) < (Rocket.Height + Enemy.Height) div 3) then
+        if Enemies[J].Collides(Rockets[I]) then
           Enemies[J].Alive := false;
 
-      Rockets[I].Y += Round(Window.Fps.UpdateSecondsPassed * 2000);
+      Rockets[I].Position += Vector2Single(0, Window.Fps.UpdateSecondsPassed * RocketSpeed);
     end;
 
-  X := Round(Position[0]);
-  Y := Round(Position[1]);
-  if //(X < 0) or (X >= MapMask.Width) or
-     //(Y < 0) or (Y >= MapMask.Height) or
-     { black }
-     (PVector3Byte(MapMask.PixelPtr(X mod MapWidth, Y mod MapHeight))^[0] = 0) then
-  begin
-    MessageOk(Window, 'Crash!');
-    Restart;
-  end;
+  { collisions player vs wall }
+  if PlayerCrashedWithWall( 0,  0) then Exit;
+  if PlayerCrashedWithWall(-1, -1) then Exit;
+  if PlayerCrashedWithWall(-1,  1) then Exit;
+  if PlayerCrashedWithWall( 1,  1) then Exit;
+  if PlayerCrashedWithWall( 1, -1) then Exit;
 
-  X := Round(Position[0] + Player.Width * 0.6);
-  Y := Round(Position[1] + Player.Height * 0.6);
-  if //(X < 0) or (X >= MapMask.Width) or
-     //(Y < 0) or (Y >= MapMask.Height) or
-     { black }
-     (PVector3Byte(MapMask.PixelPtr(X mod MapWidth, Y mod MapHeight))^[0] = 0) then
-  begin
-    MessageOk(Window, 'Crash!');
-    Restart;
-  end;
-
-  X := Round(Position[0]);
-  Y := Round(Position[1] + Player.Height * 0.6);
-  if //(X < 0) or (X >= MapMask.Width) or
-     //(Y < 0) or (Y >= MapMask.Height) or
-     { black }
-     (PVector3Byte(MapMask.PixelPtr(X mod MapWidth, Y mod MapHeight))^[0] = 0) then
-  begin
-    MessageOk(Window, 'Crash!');
-    Restart;
-  end;
-
-  X := Round(Position[0] + Player.Width * 0.75);
-  Y := Round(Position[1] + Player.Height * 0.25);
-  if //(X < 0) or (X >= MapMask.Width) or
-     //(Y < 0) or (Y >= MapMask.Height) or
-     { black }
-     (PVector3Byte(MapMask.PixelPtr(X mod MapWidth, Y mod MapHeight))^[0] = 0) then
-  begin
-    MessageOk(Window, 'Crash!');
-    Restart;
-  end;
+  { we constantly change Player.Position, to just redraw constantly }
+  Window.Invalidate;
 end;
 
 procedure WindowPress(Container: TUIContainer; const Event: TInputPressRelease);
@@ -200,11 +209,11 @@ begin
   if Event.IsKey(K_Space) then
   begin
     for I := 0 to High(Rockets) do
+      { fire first available rocket }
       if not Rockets[I].Alive then
       begin
         Rockets[I].Alive := true;
-        Rockets[I].X := Round(Position[0] + Player.Width div 2);
-        Rockets[I].Y := Round(Position[1] + Player.Height div 2);
+        Rockets[I].Position := Player.Position;
         Break;
       end;
   end;
@@ -228,19 +237,19 @@ initialization
   Application.OnInitialize := @ApplicationInitialize;
 
   { create Window and initialize Window callbacks }
-  Window := TCastleWindowTouch.Create(Application);
+  Window := TCastleWindow.Create(Application);
   Application.MainWindow := Window;
-  Window.Width := 1024; // TODO: don't hardcode
+  { for now, just hardcode the size to match map width, looks best }
+  Window.Width := 1024;
   Window.Height := 1024;
-  // todo res allowed none
+  Window.ResizeAllowed := raNotAllowed;
   Window.OnRender := @WindowRender;
-  Window.OnResize := @WindowResize;
   Window.OnUpdate := @WindowUpdate;
   Window.OnPress := @WindowPress;
 finalization
   FreeAndNil(Map);
   FreeAndNil(MapMask);
-  FreeAndNil(Player);
+  FreeAndNil(PlayerImage);
   FreeAndNil(Enemy);
   FreeAndNil(Rocket);
 end.
